03e36e72670a195232ec41b5543423e20cd58424
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-03-18 12:18:44 -0600

    feat: add cancun to hive history data file

--- portal-spec-test-utils-rs/src/lib.rs
+++ portal-spec-test-utils-rs/src/lib.rs
@@ -1,4 +1,5 @@
 // Execution Layer hard forks https://ethereum.org/en/history/
+pub const CANCUN_BLOCK_NUMBER: u64 = 19426587;
 pub const SHANGHAI_BLOCK_NUMBER: u64 = 17034870;
 pub const MERGE_BLOCK_NUMBER: u64 = 15537394;
 pub const LONDON_BLOCK_NUMBER: u64 = 12965000;
@@ -9,7 +10,9 @@ pub const BYZANTIUM_BLOCK_NUMBER: u64 = 4370000;
 pub const HOMESTEAD_BLOCK_NUMBER: u64 = 1150000;
 
 pub fn get_flair(block_number: u64) -> String {
-    if block_number > SHANGHAI_BLOCK_NUMBER {
+    if block_number > CANCUN_BLOCK_NUMBER {
+        " (post-cancun)".to_string()
+    } else if block_number > SHANGHAI_BLOCK_NUMBER {
         " (post-shanghai)".to_string()
     } else if block_number > MERGE_BLOCK_NUMBER {
         " (post-merge)".to_string()
